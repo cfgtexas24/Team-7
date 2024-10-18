65dece4c62d5b4b69382e6f1eb9f2db955e10b5d
Salem <salemiranloye@gmail.com>
2024-10-18 18:54:00 -0500

    Styling Redesign

--- src/app/apply/page.js
+++ src/app/apply/page.js
@@ -21,7 +21,7 @@ export default function Home() {
     "Clothes",
     "Life Skills",
     "Resume Writing",
-    "Joining STORM and having access to all programs",
+    "Joining STORM",
     "Ready-Up Service Center",
     "Other",
   ];
@@ -64,102 +64,128 @@ export default function Home() {
   };
 
   return (
-    <div className="w-full min-h-screen bg-gray-900 flex justify-center items-center">
+    <div className="w-full min-h-screen bg-white flex justify-center items-center">
       <form
-        className="max-w-[500px] bg-gray-800 border w-full flex flex-col p-3 rounded-md gap-4"
+        className="max-w-[600px] bg-gray-800 w-full flex flex-col p-8 rounded-md gap-4 shadow-xl"
         onSubmit={handleSubmit}
       >
-        <h3 className="font-bold text-4xl text-white">Get Help from STORM</h3>
+        <h3 className="font-bold text-2xl text-white mb-3">
+          Get Help from STORM
+        </h3>
+
         <Select>
           <SelectTrigger className="w-full">
-            <SelectValue className="text-white" placeholder="Select a reason" />
+            <SelectValue
+              className="text-white"
+              placeholder="What is the emergency"
+            />
           </SelectTrigger>
           <SelectContent>
             <SelectGroup>
               <SelectItem value="apple">Homeless</SelectItem>
-              <SelectItem value="banana">Sleeping in Car</SelectItem>
-              <SelectItem value="blueberry">No Parent/Guardian</SelectItem>
+              <SelectItem value="Sle">Sleeping in Car</SelectItem>
+              <SelectItem value="No Parent">No Parent/Guardian</SelectItem>
             </SelectGroup>
           </SelectContent>
         </Select>
 
-        <Input
-          placeholder="First Name"
-          name="firstName"
-          value={formData.firstName}
-          onChange={handleInputChange}
-        />
-        <Input
-          placeholder="Last Name"
-          name="lastName"
-          value={formData.lastName}
-          onChange={handleInputChange}
-        />
-        <Input
-          type="email"
-          placeholder="Email"
-          name="email"
-          value={formData.email}
-          onChange={handleInputChange}
-        />
-        <Input
-          type="number"
-          placeholder="Phone Number"
-          name="phoneNumber"
-          value={formData.phoneNumber}
-          onChange={handleInputChange}
-        />
+        <div className="flex flex-row flex-1 gap-2">
+          <Input
+            placeholder="First Name"
+            name="firstName"
+            value={formData.firstName}
+            onChange={handleInputChange}
+          />
+          <Input
+            placeholder="Last Name"
+            name="lastName"
+            value={formData.lastName}
+            onChange={handleInputChange}
+          />
+        </div>
+
+        <div className="flex gap-2">
+          <Input
+            type="email"
+            placeholder="Email"
+            name="email"
+            value={formData.email}
+            onChange={handleInputChange}
+          />
+          <Input
+            type="number"
+            placeholder="Phone Number"
+            name="phoneNumber"
+            value={formData.phoneNumber}
+            onChange={handleInputChange}
+          />
+          <Input
+            type="number"
+            placeholder="Age"
+            name="age"
+            className="min-w-[20px]"
+            value={formData.age}
+            onChange={handleInputChange}
+            max={99} // Allows a maximum of two digits
+          />
+        </div>
+
         <Input
           placeholder="Address"
           name="address"
           value={formData.address}
           onChange={handleInputChange}
         />
-        <Input
-          placeholder="State"
-          name="state"
-          value={formData.state}
-          onChange={handleInputChange}
-        />
-        <Input
-          placeholder="City"
-          name="city"
-          value={formData.city}
-          onChange={handleInputChange}
-        />
-        <Input
-          placeholder="Zip Code"
-          name="zipCode"
-          value={formData.zipCode}
-          onChange={handleInputChange}
-        />
-
-        <div className="flex flex-row flex-wrap gap-2">
-          {options.map((option, index) => (
-            <label
-              key={index}
-              className="flex items-center cursor-pointer"
-              onClick={() => handleOptionClick(option)}
-            >
-              <span
-                className={`h-6 w-6 mr-1 inline-block rounded ${
-                  selectedOptions.includes(option)
-                    ? "bg-white"
-                    : "border border-gray-300"
-                } transition-colors`}
-              ></span>
-              <span className="text-white">{option}</span>
-            </label>
-          ))}
+        <div className="flex flex-1 gap-2">
+          <Input
+            placeholder="City"
+            name="city"
+            value={formData.city}
+            onChange={handleInputChange}
+          />
+          <Input
+            placeholder="State"
+            name="state"
+            value={formData.state}
+            onChange={handleInputChange}
+          />
+          <Input
+            placeholder="Zip Code"
+            name="zipCode"
+            value={formData.zipCode}
+            onChange={handleInputChange}
+          />
         </div>
+
+        {/* <div className="flex flex-col gap-3">
+          <h4 className="text-white">I am interested in contacting someone about</h4>
+          <div className="flex flex-row flex-wrap gap-2">
+            {options.map((option, index) => (
+              <label
+                key={index}
+                className="flex items-center cursor-pointer bg-slate-600 p-2 rounded select-none"
+                onClick={() => handleOptionClick(option)}
+              >
+                <span
+                  className={`h-6 w-6 mr-1 inline-block rounded ${
+                    selectedOptions.includes(option)
+                      ? "bg-white"
+                      : "border border-gray-300"
+                  } transition-colors`}
+                ></span>
+                <span className="text-white">{option}</span>
+              </label>
+            ))}
+          </div>
+        </div> */}
 
         <div className="flex flex-col gap-2">
           <label className="text-white">Additional information </label>
           <Textarea
             placeholder="Message"
-            name="additionalInfo" // Add name attribute
-            value={formData.additionalInfo} // Bind value to formData
-            onChange={handleInputChange} // Add onChange handler
+            name="additionalInfo"
+            value={formData.additionalInfo}
+            onChange={handleInputChange}
           />
         </div>
 
