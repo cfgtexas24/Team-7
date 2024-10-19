a09e943ee90d7e6621ed655b77648e6db7487d2e
VICC <107821111+vicc404@users.noreply.github.com>
2024-10-19 05:30:18 -0500

    Created Form view to do Patient take in and to begni setting up emergency messaging, wil move on to finish the messaging system.

---
+++ XCode/CodeforGood/CodeforGood/FormView.swift
@@ -0,0 +1,18 @@
+//
+//  FormView.swift
+//  CodeforGood
+//
+//  Created by Victor Cadena on 10/19/24.
+//
+
+import SwiftUI
+
+struct FormView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FormView()
+}
