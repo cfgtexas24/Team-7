e7c062d49b80c5d48f819a4a9863735689c833c2
Osbaldo Puente Cerda <oxp9670@mavs.uta.edu>
2024-10-19 03:41:57 -0500

    Added commenting

--- backend/src/main/java/com/example/backend/controllers/UserController.java
+++ backend/src/main/java/com/example/backend/controllers/UserController.java
@@ -1,3 +1,9 @@
+/**
+ * This file sets the api routes for 
+ * POST, PUT, CREATE, DELETE
+ * of a User entity
+ */
+
 package com.example.backend.controllers;
 
 import java.util.List;
@@ -10,14 +16,13 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.backend.entity.User;
 import com.example.backend.services.UserService;
-
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @CrossOrigin("*")
 @RestController
