b27dfab85bf14cca8a7a392c1a394741e6d393db
VICC <107821111+vicc404@users.noreply.github.com>
2024-10-18 19:37:59 -0500

    Pushing XCode

---
+++ XCode/CodeforGood/CodeforGoodTests/CodeforGoodTests.swift
@@ -0,0 +1,17 @@
+//
+//  CodeforGoodTests.swift
+//  CodeforGoodTests
+//
+//  Created by Victor Cadena on 10/18/24.
+//
+
+import Testing
+@testable import CodeforGood
+
+struct CodeforGoodTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
