ab7ef46e85f273fc3d8ade54fba52070013f66c2
Osbaldo Puente Cerda <oxp9670@mavs.uta.edu>
2024-10-19 07:39:16 -0500

    Updated resources to take in longer body

--- backend/src/main/java/com/example/backend/entity/Resources.java
+++ backend/src/main/java/com/example/backend/entity/Resources.java
@@ -11,6 +11,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
 import jakarta.persistence.Table;
 
 @Entity
@@ -27,6 +28,7 @@ public class Resources {
     @Column(name = "link")
     private String link;
 
+    @Lob
     @Column(name = "body")
     private String body;
 
