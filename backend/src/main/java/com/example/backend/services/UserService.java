ae701f4f76fb10a4c6de5197d2266a88b36238bc
Osbaldo Puente Cerda <oxp9670@mavs.uta.edu>
2024-10-18 20:27:13 -0500

    Added UserService file, handles CRUD commands to the database

--- backend/src/main/java/com/example/backend/services/UserService.java
+++ backend/src/main/java/com/example/backend/services/UserService.java
@@ -1,19 +1,37 @@
 package com.example.backend.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.backend.entity.User;
 import com.example.backend.repository.UserRepository;
 
 @Service
 public class UserService {
     
-    private final UserRepository userRespsitory;
+    private final UserRepository userRepository;
 
-    public PersonService(UserRepository personRepository){
-        this.personRepository = personRepository;
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
     }
 
-    public Person addPerson(Person person) {
-        return personRepository.save(person);
+    public User createUser(User user) {
+        return userRepository.save(user);
     }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> getUserById(int id) {
+        return userRepository.findById(id);
+    }
+
+    public void deleteUser(int id) {
+        userRepository.deleteById(id);
+    }
+    
 }
