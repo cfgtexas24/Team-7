5c4c33a09c8adead432f239669a2f2cef5493995
Osbaldo Puente Cerda <oxp9670@mavs.uta.edu>
2024-10-19 00:28:01 -0500

    Added fetch call for events api

--- src/app/events/page.js
+++ src/app/events/page.js
@@ -1,7 +1,34 @@
+import { useEffect, useState } from "react";
+
 export default function Events() {
-    return (
-        <div>
-            Hello
-        </div>
-    );
-};
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      try {
+        const response = await fetch("http://52.91.214.247:8080/api/events/");
+        const data = await response.json();
+        setEvents(data);
+      } catch (error) {
+        console.error("Error Fetching events", error);
+      }
+    };
+
+    fetchEvents();
+  }, []);
+
+  return (
+    <div>
+      <h1>Events</h1>
+      {events.length > 0 ? (
+        <ul>
+          {events.map((event) => (
+            <li key={event.id}>{event.name}</li>
+          ))}
+        </ul>
+      ) : (
+        <p>No events found.</p>
+      )}
+    </div>
+  );
+}
