844a4790b256eaec6a6f352d48dd6a8fe7758d19
loganlieou <logan.lieou@gmail.com>
2024-10-18 19:44:58 -0500

    added navbar

---
+++ src/app/non_emergency/page.js
@@ -0,0 +1,10 @@
+import Emergency from "../emergency/page";
+import { Navbar } from "../navbar/page";
+
+export default function NonEmergency() {
+    return (
+        <main>
+            <Navbar/>
+        </main>
+    );
+}
