e4a9e2c64e2e6b5e485a4f1cd77d87868702cf0c
Osbaldo Puente Cerda <oxp9670@mavs.uta.edu>
2024-10-19 01:00:13 -0500

    Added post mapping url in resources (debug)

--- backend/src/main/java/com/example/backend/controllers/ResourcesController.java
+++ backend/src/main/java/com/example/backend/controllers/ResourcesController.java
@@ -1,14 +1,22 @@
 package com.example.backend.controllers;
 
-import com.example.backend.entity.Resources;
-import com.example.backend.services.ResourcesService;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.backend.entity.Resources;
+import com.example.backend.services.ResourcesService;
 
 @CrossOrigin("*")
 @RestController
@@ -38,7 +46,7 @@ public class ResourcesController {
     // }
 
     // Create a new resource
-    @PostMapping
+    @PostMapping("/")
     public Resources createResource(@RequestBody Resources resource) {
         return resourcesService.createResource(resource);
     }
