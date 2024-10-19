2aeb2b66b51070003a62479359e3036ba86a7ea3
Salem <salemiranloye@gmail.com>
2024-10-18 21:04:36 -0500

    redesign non_emergency page

--- src/app/non_emergency/page.js
+++ src/app/non_emergency/page.js
@@ -1,10 +1,22 @@
-import Emergency from "../emergency/page";
-import { Navbar } from "../navbar/page";
+import { Book, Calendar, Presentation } from "lucide-react";
 
 export default function NonEmergency() {
-    return (
-        <main>
-            <Navbar/>
-        </main>
-    );
+  return (
+    <div className="w-full min-h-screen flex justify-center">
+      <div className="flex flex-row gap-8 border-2 border-black p-8 rounded-lg shadow-lg bg-white h-fit mt-4">
+        <div className="flex flex-col items-center gap-2 transition-transform transform hover:scale-110">
+          <Book className="h-12 w-12 text-black" />
+          <span className="font-bold text-xl">Resources</span>
+        </div>
+        <div className="flex flex-col items-center gap-2 transition-transform transform hover:scale-110">
+          <Calendar className="h-12 w-12 text-black" />
+          <span className="font-bold text-xl">Events</span>
+        </div>
+        <div className="flex flex-col items-center gap-2 transition-transform transform hover:scale-110">
+          <Presentation className="h-12 w-12 text-black" />
+          <span className="font-bold text-xl">Life Lessons</span>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/app/portal/page.js
+++ src/app/portal/page.js
@@ -1,10 +1,9 @@
 import Emergency from "../emergency/page";
-import { Navbar } from "../navbar/page";
 
 export default function NonEmergency() {
     return (
         <main>
-            <Navbar/>
+
         </main>
     );
 }
